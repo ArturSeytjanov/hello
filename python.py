864f6d70cb5759d263e3bc948a98fd58400fcf5d
ArturSeytjanov <124263029+ArturSeytjanov@users.noreply.github.com>
2023-02-04 16:09:12 +0500

    Create python.py

---
+++ python.py
@@ -0,0 +1,24 @@
+a = int(input('a= '))
+b = str(a)
+if len(b)==1:
+    print('1 xanali san')
+elif len(b)==2:
+    print('2 xanali san')
+elif len(b)==3:
+    print('3 xanali san')
+elif len(b)==4:
+    print('4 xanali san')
+elif len(b)==5:
+    print('5 xanali san')
+elif len(b)==6:
+    print('6 xanali san')
+elif len(b)==7:
+    print('7 xanali san')
+elif len(b)==8:
+    print('8 xanali san')
+elif len(b)==9:
+    print('9 xanali san')
+elif len(b)==10:
+    print('10 xanali san')
+else:
+    print('Error')
